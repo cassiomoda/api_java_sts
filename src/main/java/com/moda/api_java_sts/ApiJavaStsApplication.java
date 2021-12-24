7c95cefb205dc958245632a032cd54600b87f1da
Cassio Moda <cassiomoda@gmail.com>
2021-12-24 12:14:35 -0300

    Criando o projeto

---
+++ src/main/java/com/moda/api_java_sts/ApiJavaStsApplication.java
@@ -0,0 +1,13 @@
+package com.moda.api_java_sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moda/api_java_sts/ApiJavaStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moda.api_java_sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
