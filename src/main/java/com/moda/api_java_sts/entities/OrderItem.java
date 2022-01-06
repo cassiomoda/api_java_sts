e20904bc5095cde92dcbfb9e76f38cd776caead5
Cassio Moda <cassiomoda@gmail.com>
2022-01-05 21:33:35 -0300

    Iniciada criacao da Classe OrderItem

---
+++ src/main/java/com/moda/api_java_sts/entities/OrderItem.java
@@ -0,0 +1,85 @@
+package com.moda.api_java_sts.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.moda.api_java_sts.entities.pk.OrderItemPK;
+
+@Entity
+@Table(name = "tb_Order_Item")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private OrderItemPK id;
+	private Integer quantity;
+	private Double price;
+	
+	public OrderItem() {
+		super();
+	}
+	
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		super();
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	public void setOrder(Order order) {
+		this.id.setOrder(order);
+	}
+	
+	public Order getOrder() {
+		return this.id.getOrder();
+	}
+	
+	public void setProduct(Product product) {
+		this.id.setProduct(product);
+	}
+	
+	public Product getProduct() {
+		return this.id.getProduct();
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	public Integer getQuantity() {
+		return this.quantity;
+	}
+	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public Double getPrice() {
+		return this.price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+	
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+}
