0baf1f668523fbe034d3213c6b645ff4f99f2535
Cassio Moda <cassiomoda@gmail.com>
2022-01-11 17:52:13 -0300

    Implementado tratamento de exception para recursos nao encontrados

---
+++ src/main/java/com/moda/api_java_sts/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.moda.api_java_sts.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.moda.api_java_sts.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError stdError  = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(stdError);
+	}
+}
---
+++ src/main/java/com/moda/api_java_sts/resources/exceptions/StandardError.java
@@ -0,0 +1,69 @@
+package com.moda.api_java_sts.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+	
+	public StandardError() {
+	}
+	
+	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+	
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+	public Instant getTimestamp() {
+		return this.timestamp;
+	}
+	
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	public Integer getStatus() {
+		return this.status;
+	}
+	
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	public String getError() {
+		return this.error;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public String getPath() {
+		return this.path;
+	}
+}
