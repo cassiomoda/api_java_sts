9bddd7d07881d9d39d2385a1af59afb580a86767
Cassio Moda <cassiomoda@gmail.com>
2022-01-12 11:30:32 -0300

    Adicionados tratamentos de exception para os metodos de delete e de upate do usuario

--- src/main/java/com/moda/api_java_sts/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/moda/api_java_sts/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.moda.api_java_sts.services.exceptions.DatabaseException;
 import com.moda.api_java_sts.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +20,14 @@ public class ResourceExceptionHandler {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError stdError  = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(stdError);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError stdError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(stdError);
 	}
 }
--- src/main/java/com/moda/api_java_sts/services/UserService.java
+++ src/main/java/com/moda/api_java_sts/services/UserService.java
@@ -3,11 +3,16 @@ package com.moda.api_java_sts.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.moda.api_java_sts.entities.User;
 import com.moda.api_java_sts.repositories.UserRepository;
+import com.moda.api_java_sts.services.exceptions.DatabaseException;
 import com.moda.api_java_sts.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +35,15 @@ public class UserService {
 	}
 	
 	public void deleteById(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	private void updateData(User userDb, User user) {
@@ -41,8 +54,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User user) {
-		User userDb = userRepository.getById(id);
-		updateData(userDb, user);
-		return userRepository.save(userDb);
+		try {
+			User userDb = userRepository.getById(id);
+			updateData(userDb, user);
+			return userRepository.save(userDb);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 }
